bd569227ac367a1a024654f9c66d687ee3951779
AmingWu <38445175+AmingWu@users.noreply.github.com>
2019-10-07 11:09:00 +0800

    Add files via upload

--- model/CNN_MODEL.py
+++ model/CNN_MODEL.py
@@ -303,3 +303,230 @@ def forward_feature(feature, caption, mask, dropout, first_attention, fouth_atte
     #loss = 0.3 * recon_loss + loss
 
     return result, loss
+
+def forward_test(feature, first_attention, fouth_attention, fusion_para, Encode, \ 
+    Dilation, word_embed, embed_bias, dropout=1.0, batch_size=1):
+
+    #encoder
+    process_feature = feature
+    feature_set = encode_feature(process_feature)
+    #feature_set = process_feature
+    fea4 = tf.nn.conv1d(feature_set, Encode['1_1_w'], 1, 'SAME') + Encode['1_1_b']
+    fea4 = tf.nn.relu(fea4)
+
+    feature3 = fea4
+    #dilation decoder
+    loss = 0
+    result = []
+    h = tf.reduce_mean(fea4, 1)
+    h = tf.expand_dims(h, 1)
+    first_layer = []
+    second_layer = []
+    third_layer = []
+    fouth_layer = []
+    zero_layer = []
+    v_shape = fea4.get_shape().as_list()
+    #hidden, cell = encode(fea4, dropout)
+    rnn_attention = tf.reduce_mean(feature3, 1)
+    x = tf.zeros([v_shape[0], v_shape[2]])
+    for i in range(17):
+        if i == 0:
+
+            input_f = x + rnn_attention
+            input_f = tf.expand_dims(input_f, 1)
+            #input_f = tf.nn.relu(input_f)
+            zero_layer.append(input_f)
+            shape = zero_layer[i].get_shape().as_list()
+            pad = tf.zeros([shape[0], 1, shape[2]])
+            new_input = tf.concat([pad, zero_layer[i]], 1)
+            f1 = tf.nn.conv1d(new_input, Dilation['0'], 1, 'VALID') + Dilation['0_b']
+            f1 = tf.nn.sigmoid(f1)
+            f2 = tf.nn.conv1d(new_input, Dilation['0_n'], 1, 'VALID') + Dilation['0_b_n']
+            f2 = tf.tanh(f2)
+            f = f1 * f2
+            hidden = tf.squeeze(f, 1)
+
+            att_feature = attention(tf.expand_dims(hidden, 1), feature3, first_attention)
+
+            first_fusion = tf.concat([tf.expand_dims(hidden, 1), att_feature], 2)
+            first_fusion = tf.nn.conv1d(first_fusion, channel_fusion['finnal'], 1, 'SAME')
+            first_layer.append(first_fusion)
+            #first_layer.append(tf.expand_dims(hidden, 1) + att_feature)
+
+            input_f = first_layer[i]
+            shape = input_f.get_shape().as_list()
+            pad = tf.zeros([shape[0], 1, shape[2]])
+            new_input = tf.concat([pad, input_f], 1)
+            f1 = tf.nn.conv1d(new_input, Dilation['1'], 1, 'VALID') + Dilation['1_b']
+            f1 = tf.nn.sigmoid(f1)
+            f2 = tf.nn.conv1d(new_input, Dilation['1_n'], 1, 'VALID') + Dilation['1_b_n']
+            f2 = tf.tanh(f2)
+            f = f1 * f2
+
+            second_layer.append(f)
+            shape = f.get_shape().as_list()
+            m1 = tf.zeros([shape[0], 1, shape[2]])
+            m2 = f
+            m = tf.concat([m1, m2], 1)
+            f1 = tf.nn.conv1d(m, Dilation['2'], 1, 'VALID') + Dilation['2_b']
+            f1 = tf.nn.sigmoid(f1)
+            f2 = tf.nn.conv1d(m, Dilation['2_n'], 1, 'VALID') + Dilation['2_b_n']
+            f2 = tf.tanh(f2)
+            f = f1 * f2
+
+            third_layer.append(f)
+            shape = f.get_shape().as_list()
+            m1 = tf.zeros([shape[0], 1, shape[2]])
+            m2 = f
+            m = tf.concat([m1, m2], 1)
+            f1 = tf.nn.conv1d(m, Dilation['3'], 1, 'VALID') + Dilation['3_b']
+            f1 = tf.nn.sigmoid(f1)
+            f2 = tf.nn.conv1d(m, Dilation['3_n'], 1, 'VALID') + Dilation['3_b_n']
+            f2 = tf.tanh(f2)
+            f = f1 * f2
+            att_feature = attention(f, feature3, fouth_attention)
+
+            fouth_layer.append(f + att_feature)
+            shape = f.get_shape().as_list()
+            m1 = tf.zeros([shape[0], 1, shape[2]])
+            m2 = f
+            m = tf.concat([m1, m2], 1)
+            f1 = tf.nn.conv1d(m, Dilation['4'], 1, 'VALID') + Dilation['4_b']
+            f1 = tf.nn.sigmoid(f1)
+            f2 = tf.nn.conv1d(m, Dilation['4_n'], 1, 'VALID') + Dilation['4_b_n']
+            f2 = tf.tanh(f2)
+            f = f1 * f2
+
+            word_in = f + att_feature
+            word = tf.matmul(word_in[:, 0, :], word_embed['d_embed']) + embed_bias['bias_dembed']
+            word = tf.nn.softmax(word)
+            result.append(word)
+
+            x1 = word
+            w_s = tf.squeeze(word, 0)
+            w_p = tf.argmax(w_s)
+            x1 = tf.one_hot(w_p, dic_num)
+            x1 = tf.expand_dims(x1, 0)
+            x = tf.matmul(x1, word_embed['embed']) + embed_bias['bias_embed']
+        else:
+            input_f=modal_fusion(x, rnn_attention, fusion_para, dropout)
+            input_f=tf.expand_dims(input_f, 1)
+            #input_f=tf.nn.relu(input_f)
+
+            zero_layer.append(input_f)
+            m1 = zero_layer[i-1]
+            m2 = zero_layer[i]
+            new_input = tf.concat([m1, m2], 1)
+            f1 = tf.nn.conv1d(new_input, Dilation['0'], 1, 'VALID') + Dilation['0_b']
+            f1 = tf.nn.sigmoid(f1)
+            f2 = tf.nn.conv1d(new_input, Dilation['0_n'], 1, 'VALID') + Dilation['0_b_n']
+            f2 = tf.tanh(f2)
+            f = f1 * f2
+            hidden = tf.squeeze(f, 1)
+
+            att_feature = attention(tf.expand_dims(hidden, 1), feature3, first_attention)
+
+            first_fusion = tf.concat([tf.expand_dims(hidden, 1), att_feature], 2)
+            first_fusion = tf.nn.conv1d(first_fusion, channel_fusion['finnal'], 1, 'SAME')
+            first_layer.append(first_fusion)
+            #first_layer.append(tf.expand_dims(hidden, 1) + att_feature)
+
+            m1 = first_layer[i-1]
+            m2 = first_layer[i]
+            new_input = tf.concat([m1, m2], 1)
+            f1 = tf.nn.conv1d(new_input, Dilation['1'], 1, 'VALID') + Dilation['1_b']
+            f1 = tf.nn.sigmoid(f1)
+            f2 = tf.nn.conv1d(new_input, Dilation['1_n'], 1, 'VALID') + Dilation['1_b_n']
+            f2 = tf.tanh(f2)
+            f = f1 * f2
+            second_layer.append(f)
+
+            if i - 2 < 0:
+                shape = f.get_shape().as_list()
+                pad = tf.zeros([shape[0], 1, shape[2]])
+                new_input = tf.concat([pad, second_layer[i]], 1)
+                f1 = tf.nn.conv1d(new_input, Dilation['2'], 1, 'VALID') + Dilation['2_b']
+                f1 = tf.nn.sigmoid(f1)
+                f2 = tf.nn.conv1d(new_input, Dilation['2_n'], 1, 'VALID') + Dilation['2_b_n']
+                f2 = tf.tanh(f2)
+                f = f1 * f2
+                third_layer.append(f)
+            else:
+                m1 = second_layer[i-2]
+                m2 = second_layer[i]
+                new_input = tf.concat([m1, m2], 1)
+                f1 = tf.nn.conv1d(new_input, Dilation['2'], 1, 'VALID') + Dilation['2_b']
+                f1 = tf.nn.sigmoid(f1)
+                f2 = tf.nn.conv1d(new_input, Dilation['2_n'], 1, 'VALID') + Dilation['2_b_n']
+                f2 = tf.tanh(f2)
+                f = f1 * f2
+                third_layer.append(f)
+
+            att_feature = 0
+            if i - 4 < 0:
+                shape = f.get_shape().as_list()
+                pad = tf.zeros([shape[0], 1, shape[2]])
+                new_input = tf.concat([pad, third_layer[i]], 1)
+                f1 = tf.nn.conv1d(new_input, Dilation['3'], 1, 'VALID') + Dilation['3_b']
+                f1 = tf.nn.sigmoid(f1)
+                f2 = tf.nn.conv1d(new_input, Dilation['3_n'], 1, 'VALID') + Dilation['3_b_n']
+                f2 = tf.tanh(f2)
+                f = f1 * f2
+                att_feature = attention(f, feature3, fouth_attention)
+                fouth_layer.append(f + att_feature)
+            else:
+                m1 = third_layer[i-4]
+                m2 = third_layer[i]
+                new_input = tf.concat([m1, m2], 1)
+                f1 = tf.nn.conv1d(new_input, Dilation['3'], 1, 'VALID') + Dilation['3_b']
+                f1 = tf.nn.sigmoid(f1)
+                f2 = tf.nn.conv1d(new_input, Dilation['3_n'], 1, 'VALID') + Dilation['3_b_n']
+                f2 = tf.tanh(f2)
+                f = f1 * f2
+                att_feature = attention(f, feature3, fouth_attention)
+                fouth_layer.append(f + att_feature)
+
+            if i - 2 < 0:
+                shape = f.get_shape().as_list()
+                pad = tf.zeros([shape[0], 1, shape[2]])
+                new_input = tf.concat([pad, fouth_layer[i]], 1)
+                f1 = tf.nn.conv1d(new_input, Dilation['4'], 1, 'VALID') + Dilation['4_b']
+                f1 = tf.nn.sigmoid(f1)
+                f2 = tf.nn.conv1d(new_input, Dilation['4_n'], 1, 'VALID') + Dilation['4_b_n']
+                f2 = tf.tanh(f2)
+                f = f1 * f2
+
+                word_in = f + att_feature
+                word = tf.matmul(word_in[:,0,:], word_embed['d_embed']) + embed_bias['bias_dembed']
+                word = tf.nn.softmax(word)
+                result.append(word)
+
+                x1 = word
+                w_s = tf.squeeze(word, 0)
+                w_p = tf.argmax(w_s)
+                x1 = tf.one_hot(w_p, dic_num)
+                x1 = tf.expand_dims(x1, 0)
+                x = tf.matmul(x1, word_embed['embed']) + embed_bias['bias_embed']
+            else:
+                m1 = fouth_layer[i-2]
+                m2 = fouth_layer[i]
+                new_input = tf.concat([m1, m2], 1)
+                f1 = tf.nn.conv1d(new_input, Dilation['4'], 1, 'VALID') + Dilation['4_b']
+                f1 = tf.nn.sigmoid(f1)
+                f2 = tf.nn.conv1d(new_input, Dilation['4_n'], 1, 'VALID') + Dilation['4_b_n']
+                f2 = tf.tanh(f2)
+                f = f1 * f2
+
+                word_in = f + att_feature
+                word = tf.matmul(word_in[:,0,:], word_embed['d_embed']) + embed_bias['bias_dembed']
+                word = tf.nn.softmax(word)
+                result.append(word)
+
+                x1 = word
+                w_s = tf.squeeze(word, 0)
+                w_p = tf.argmax(w_s)
+                x1 = tf.one_hot(w_p, dic_num)
+                x1 = tf.expand_dims(x1, 0)
+                x = tf.matmul(x1, word_embed['embed']) + embed_bias['bias_embed']
+
+    return result
